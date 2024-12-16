67fe3fbd3a6545ff0dc1738947fcdb8639b1943b
MathNosseb <mathnosseb@gmail.com>
2024-12-16 18:57:37 +0100

    Mofify ReadMe file

--- run.sh
+++ run.sh
@@ -1,16 +1,24 @@
-read -p "enter commit   " commit
-git commit -m commit
-git push
+read -p "push? (y/n) " query
+if [ "$query" == "y" ]; then
+    git add .
+    read -p "Enter commit message: " commit
+    git commit -m "$commit"
+    git push
+fi
 
-export PATH=$PATH:/usr/local/i386elfgcc/bin
+read -p "run script?    (y/n)   " query
+if [ "$query" == "y" ]; then
 
-nasm "boot.asm" -f bin -o "Binaries/boot.bin"
-nasm "kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
-i386-elf-gcc -ffreestanding -m32 -g -c "kernel.c" -o "Binaries/kernel.o"
-nasm "zeroes.asm" -f bin -o "Binaries/zeroes.bin"
+    export PATH=$PATH:/usr/local/i386elfgcc/bin
 
-i386-elf-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
+    nasm "bootloader/boot.asm" -f bin -o "Binaries/boot.bin"
+    nasm "bootloader/kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
+    i386-elf-gcc -ffreestanding -m32 -g -c "Kernel/kernel.c" -o "Binaries/kernel.o"
+    nasm "bootloader/zeroes.asm" -f bin -o "Binaries/zeroes.bin"
 
-cat "Binaries/boot.bin" "Binaries/full_kernel.bin" "Binaries/zeroes.bin"  > "Binaries/OS.bin"
+    i386-elf-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
 
-qemu-system-x86_64 -drive format=raw,file="Binaries/OS.bin",index=0,if=floppy,  -m 128M
+    cat "Binaries/boot.bin" "Binaries/full_kernel.bin" "Binaries/zeroes.bin"  > "Binaries/OS.bin"
+
+    qemu-system-x86_64 -drive format=raw,file="Binaries/OS.bin",index=0,if=floppy,  -m 128M
+fi

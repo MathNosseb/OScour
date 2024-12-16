024825fdd0789e4e11b9e12f8935721ea8ed4c5b
MathNosseb <mathnosseb@gmail.com>
2024-12-16 22:26:50 +0100

    ajout du fprint dans un autre fichier

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,4 +1,20 @@
+/*
+Explication de la mémoire vidéo
+index linéaire = y * 80 + x
+ou y est la ligne (0 à 24)
+ou x est la colonne (0 à 79)
+
+dimension de l'écran en x86 : 80x25
+
+adresse mémoire vidéo = 0xB8000 + (index linéaire * 2)
+
+*/
+
+//on call les fonctions pour les tests
+extern void fprint(const char *str);
+
 void _start(){
-    *(char*)0xb8000 = 'Q';
-    return;
+    fprint("coucouc");
 }
+
+

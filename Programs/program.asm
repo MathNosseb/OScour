9220ec0bd4be4c3ddb45c1272549f1e5c11f0595
MathNosseb <m.besson2008@gmail.com>
2026-06-25 19:42:21 +0200

    ajout d'un code plus compréhensible pour la gestion de lancement de programmes externes utilisant le print

--- Programs/program.asm
+++ Programs/program.asm
@@ -1,12 +1,17 @@
 [bits 32]
-call next
-next:
+call start
+start:
+    ;prend les 4 derniers octet de la pile (adresse de la fonction)
+    ; les places dans ebx
+    ;et les retires de la pile (add esp 4)
     pop ebx
-    add ebx, hello - next
+    add ebx, hello
+    sub ebx, 5
+    ;il faut mettre l adresse de hello
     push ebx
-    mov eax, [0x504]
+    mov eax, [0x504];la fonction qui a son adresse a 0x504 -> print
     call eax
     add esp, 4
     ret
 
-hello db "Hello from external program", 0
+hello db "Hello World",0
